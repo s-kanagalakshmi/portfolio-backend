a7ba45ee720a7625eaefde9f67e092637a964887
s-kanagalakshmi <kanagalakshmi.mca16@gmail.com>
2025-05-04 20:47:21 +0530

    changes

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+app.use(cors())
+// app.use(cors({
+//   origin: "https://portfolio-frontend-five-khaki.vercel.app/",
+// }));
 
 app.use(express.json());
 app.use("/api/blogs", blogRoutes);
